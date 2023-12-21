b0c284fde7df36e307f0f83c644df99236224857
Adib-96 <adibbh1996@gmail.com>
2023-12-21 19:45:57 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -12,10 +12,12 @@ if __name__ == "__main__":
 
     employee_id = argv[1]
     try:
-        req_employee = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+        req_employee = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{employee_id}")
         req_employee.raise_for_status()
 
-        req_todo = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+        req_todo = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
         req_todo.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
@@ -25,7 +27,9 @@ if __name__ == "__main__":
     todos = req_todo.json()
 
     filename = f"{employee_id}.json"
-    data = {employee_id: [{"task": todo["title"], "completed": todo["completed"], "username": username} for todo in todos]}
+    data = {employee_id: [{"task": todo["title"],
+                           "completed": todo["completed"],
+                           "username": username} for todo in todos]}
 
     with open(filename, "w") as f:
         dump(data, f, indent=2)
