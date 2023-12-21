26efcf844ab92551c74689c31fa0382148e0568c
Adib-96 <adibbh1996@gmail.com>
2023-12-21 19:54:40 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,23 +4,34 @@ for a given employee ID, returns information about
 his/her TODO list progress."""
 import requests
 from sys import argv
+
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: {} <employee_id>".format(argv[0]))
+        exit(1)
+
     employee_id = argv[1]
-    req_name = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-    req_name = req_name.json()
-    name = req_name["name"]
-    req_todo = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            employee_id))
-    req_todo = req_todo.json()
-    completed_tasks = 0
-    for i in req_todo:
-        if i["completed"]:
-            completed_tasks += 1
-    print("Employee {} is done with tasks({}/{}):".format(name,
-                                                          completed_tasks,
-                                                          len(req_todo)))
-    for i in req_todo:
-        if i["completed"]:
-            print("\t {}".format(i["title"]))
+
+    try:
+        # Fetch user information
+        user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+        user_response.raise_for_status()
+        user_data = user_response.json()
+        name = user_data["name"]
+
+        # Fetch user's TODO list
+        todo_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+        todo_response.raise_for_status()
+        todo_data = todo_response.json()
+
+        # Count completed tasks and print information
+        completed_tasks = sum(1 for todo in todo_data if todo["completed"])
+        print("Employee {} is done with tasks ({}/{}):".format(name, completed_tasks, len(todo_data)))
+
+        # Print completed task titles
+        for todo in (todo for todo in todo_data if todo["completed"]):
+            print("\t{}".format(todo["title"]))
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        exit(1)
