7da728af79e1731400ffa142fb3508fdea4fcc2c
Adib-96 <adibbh1996@gmail.com>
2023-12-21 19:39:21 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,26 +11,28 @@ def exportJson():
     """
     function that use an api to format datas
     """
-    user_id = argv[1]
-    json_file = f"{user_id}.json"
-
-    req_name = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    employee_id = argv[1]
+    req_employee = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    req_employee = req_employee.json()
+    username = req_employee["username"]
     req_todo = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            user_id))
-
-    username_value = req_name.json()['username']
-    user_data = req_todo.json()
-
-    my_array = []
-    for el in user_data:
-        my_array.append({"task": "{}".format(el['title']),
-                         "completed": "{}".format(
-            el['completed']), "username": "{}".format(username_value)})
-    format_ready_for_json = json.dumps({"{}".format(user_id): my_array})
-    with open(json_file, "w") as js_f:
-        js_f.write(format_ready_for_json)
+            employee_id))
+    req_todo = req_todo.json()
+    filename = "{}.json".format(employee_id)
+    nested_dic = {}
+    my_list = []
+    
+    for i in req_todo:
+        dict = {
+            "task": i["title"],
+            "completed": i["completed"],
+            "username": username}
+        my_list.append(dict)
+    nested_dic[employee_id] = my_list
+    with open(filename, "w") as f:
+        json.dump(nested_dic, f)
 
 
 if __name__ == "__main__":
