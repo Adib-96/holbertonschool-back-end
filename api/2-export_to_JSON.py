216ec03fa494cd93bfe00dc1022c8b5bd1d4656a
Adib-96 <adibbh1996@gmail.com>
2023-12-21 19:45:21 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,25 +4,28 @@ extend your Python script to export data in the JSON format."""
 import requests
 from sys import argv
 from json import dump
+
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: {} <employee_id>".format(argv[0]))
+        exit(1)
+
     employee_id = argv[1]
-    req_employee = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-    req_employee = req_employee.json()
-    username = req_employee["username"]
-    req_todo = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            employee_id))
-    req_todo = req_todo.json()
-    filename = "{}.json".format(employee_id)
-    nested_dic = {}
-    ls = []
-    for i in req_todo:
-        dict = {
-            "task": i["title"],
-            "completed": i["completed"],
-            "username": username}
-        ls.append(dict)
-    nested_dic[employee_id] = ls
+    try:
+        req_employee = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+        req_employee.raise_for_status()
+
+        req_todo = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+        req_todo.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        exit(1)
+
+    username = req_employee.json()["username"]
+    todos = req_todo.json()
+
+    filename = f"{employee_id}.json"
+    data = {employee_id: [{"task": todo["title"], "completed": todo["completed"], "username": username} for todo in todos]}
+
     with open(filename, "w") as f:
-        dump(nested_dic, f)
+        dump(data, f, indent=2)
