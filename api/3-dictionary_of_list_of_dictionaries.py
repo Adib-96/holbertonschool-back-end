912c1468d0d3877c133a7b29771eea62f6464606
adib <121905691+Adib-96@users.noreply.github.com>
2024-03-21 13:10:38 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
-"""Using what you did in the task #0,
-extend your Python script to export data in the JSON format."""
+"""Python script to export data in the JSON format."""
+
+import json
 import requests
 from sys import argv
-from json import dump
 
 
-def get_user_data():
-    """Fetch user data from the API."""
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    return requests.get(users_url).json()
+if __name__ == '__main__':
+    main_url = 'https://jsonplaceholder.typicode.com'
+    users_url = main_url + "/users"
+    todos_url = main_url + "/todos"
 
+    users_result = requests.get(users_url).json()
+    todos_result = requests.get(todos_url).json()
 
-def get_todo_data():
-    """Fetch todo data from the API."""
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-    return requests.get(todos_url).json()
+    json_dic = {}
+    for user in users_result:
+        user_id = user['id']
 
+        if user_id not in json_dic:
+            json_dic[user_id] = []
 
-def export_data_to_json(users, todos, filename="todo_all_employees.json"):
-    """Export data to JSON file."""
-    nested_dict = {}
+        for todo in todos_result:
+            internal_dic = {}
+            if user_id == todo['userId']:
+                internal_dic["username"] = user['username']
+                internal_dic["task"] = todo['title']
+                internal_dic["completed"] = todo['completed']
+                json_dic[user_id].append(internal_dic)
 
-    for user in users:
-        user_id = user["id"]
-        user_name = user["username"]
-
-        user_tasks = [
-            {"task": task["title"],
-             "completed": task["completed"], "username": user_name}
-            for task in todos if task["userId"] == user_id
-        ]
-
-        nested_dict[user_id] = user_tasks
-
-    with open(filename, "w") as f:
-        dump(nested_dict, f, indent=2)
-
-
-if __name__ == "__main__":
-    users_data = get_user_data()
-    todos_data = get_todo_data()
-
-    export_data_to_json(users_data, todos_data)
+    with open(f"todo_all_employees.json", 'w') as j_file:
+        json.dump(json_dic, j_file)
